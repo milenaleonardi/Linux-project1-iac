715eea7b55e3bbdcb8c9708431bd6d5f49aef66f
Milena Vargas Leonardi <milena.leonardi@hotmail.com>
2023-03-04 22:36:07 -0300

    Create IaC-v2.sh

---
+++ IaC-v2.sh
@@ -0,0 +1,15 @@
+# Script para automatizar instalação do apache para criar servidores web com o terminal Linux Ubuntu/Debian
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site dio/archive/refs/heads/main.zip
+unzip main.zip
+
+cd linux-site-dio-main
+cp -R * /var/www/html/
